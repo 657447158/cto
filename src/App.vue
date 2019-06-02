4d9511e3cd2325ab0eaaf0c34d7c142285590c01
657447158 <657447158@qq.com>
2019-06-02 20:40:47 +0800

    个人中心

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-      <router-view></router-view>
-      <Footer />
+    <router-view></router-view>
+	  <Footer></Footer>
   </div>
 </template>
 
---
+++ src/pages/personal/personal-wechat.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wechat">
+        <div class="wechat-item">
+            <span class="label">微信号</span>
+            <input type="text" placeholder="请输入微信号" />
+        </div>
+        <div class="wechat-item">
+            <span class="label">真实姓名</span>
+            <input type="text" placeholder="请输入姓名" />
+        </div>
+        <p class="tips">(微信收款码的真实姓名)</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    .wechat {
+        padding: 0 .32rem;
+        background: $bg02;
+        &-item {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 1.14rem;
+            &:first-child {
+                border-bottom: 1px solid $border01;
+            }
+        }
+        .label {
+            font-size: $f32;
+            font-weight: bold;
+            color: $fc11;
+        }
+        input {
+            width: 4.7rem;
+            text-align: right;
+        }
+        .tips {
+            color: #eb7219;
+            font-size: $f26;
+        }
+    }
+</style>
