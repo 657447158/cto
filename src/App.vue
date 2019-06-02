2d86bc08ff7dfe461670222fc50c7209d591c39f
Bocai <bocaijs@126.com>
2019-06-02 20:33:05 +0800

    添加路由监听

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
       <router-view></router-view>
-      <Footer />
+      <Footer v-if="showFooter"/>
   </div>
 </template>
 
 <script>
-  import Footer from './components/footer'
+  import Footer from './components/footer';
+
+
+  const ORDER = 'order';
+  const INDEX = 'index';
+  const PERSONAL ='personal';
   export default {
     components: {
       Footer
+    },
+    data(){
+      return {
+         showFooter:true,
+      }
+    },
+    watch:{
+      $route( to , from ){   
+       console.log( to )
+       if(to.name === ORDER || to.name ===INDEX || to.name === PERSONAL){
+         this.showFooter = true;
+       }else{
+         this.showFooter = false;
+       }
+     }
     }
   }
 </script>
