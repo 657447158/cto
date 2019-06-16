30925552bec5b0938e131a3cd65ac7afe1a25c09
657447158 <657447158@qq.com>
2019-06-16 11:22:59 +0800

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-  <div id="app">
-      <router-view></router-view>
-      <Footer v-if="showFooter"/>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+		<Footer v-if="showFooter"/>
+	</div>
 </template>
 
 <script>
-  import Footer from './components/footer';
-
-  const ORDER = 'order';
-  const INDEX = 'index';
-  const PERSONAL ='personal';
-  export default {
+import Footer from './components/footer';
+const ORDER = 'order';
+const INDEX = 'index';
+const PERSONAL ='personal';
+export default {
     components: {
-		  Footer
+		Footer
     },
     data () {
-      return {
-        showFooter:true,
-      }
+		return {
+			showFooter:true,
+		}
     },
     watch:{
-      $route(to){   
-        if(to.name === ORDER || to.name ===INDEX || to.name === PERSONAL){
-          this.showFooter = true;
-        } else {
-          this.showFooter = false;
-        }
-      }
-    }
-  }
+		$route(to){   
+			if(to.name === ORDER || to.name ===INDEX || to.name === PERSONAL){
+				this.showFooter = true;
+			} else {
+				this.showFooter = false;
+			}
+		}
+	}
+}
 </script>
